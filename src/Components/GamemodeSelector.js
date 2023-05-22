d99fab7a836aaa4f0bde5ef637ae674cbfcc8dc4
Harry England <harryengland666@gmail.com>
2023-05-22 21:55:08 +0100

    Few Fixes

--- src/Components/GamemodeSelector.js
+++ src/Components/GamemodeSelector.js
@@ -16,7 +16,7 @@ const GamemodeSelector = ( {onClick, playerSelect, firsttothree} ) => {
   return (
     <div className= "helpbox">
             <div className='helpcont'>
-                <h3>Please select a gamemode:</h3>
+                <h3>Please Select A Gamemode:</h3>
                 <div className='gamemode'>
                   <button className='gm' onClick={handleSingleMatch}>Single Match</button>
                   <p>Player vs. Player</p>  
