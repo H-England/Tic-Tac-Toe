730adedecad755c5e51b0ab717c81a712f6c69ad
Harry England <harryengland666@gmail.com>
2023-07-11 19:22:35 +0100

    First Implementation of Algorithm (Untested)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,108 @@ import { WinCondition } from './WinConditions';
 import WinMessage from './Components/WinMessage';
 import RoundWinMessage from './Components/RoundWinMessage';
 
+
+function calculateBestMove(board, depth, maximizingPlayer) {
+  if(checkWin(board)){
+    if(maximizingPlayer){
+      return {score: -1 }
+    } else {
+      return {score: 1 }
+    }
+  } else if (checkDraw(board)) {
+    return {score: 0 }
+  }
+
+  const moves = []
+
+  for(let i = 0; i < board.length; i++){
+    if(board[i] === ""){
+
+      const move = {}
+      move.index = i
+
+      board[i] = maximizingPlayer ? "O" : "X"
+
+      const result = calculateBestMove(board, depth + 1, !maximizingPlayer)
+      move.score = result.score
+      
+      board[i] = ""
+
+      moves.push(move)
+    }
+  }
+
+  let bestMove
+  if(maximizingPlayer){
+    let bestScore = -Infinity
+    for(let i = 0; i < moves.length; i++){
+      if(moves[i].score > bestScore){
+        bestScore = moves[i].score
+        bestMove = i
+      }
+    }
+  } else {
+    let bestScore = Infinity
+    for(let i = 0; i < moves.length; i++ ) {
+      if(moves[i].score < bestScore) {
+        bestScore = moves[i].score
+        bestMove = i
+      }
+    }
+  }
+
+  return moves[bestMove]
+
+}
+
+  function checkWin(board){
+    for(let i = 0; i < 9; i += 3){
+      if(
+        board[i] !== "" &&
+        board[i] === board[i + 1] &&
+        board[i] === board[i + 2]
+      ){
+        return true
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i] !== "" &&
+        board[i] === board[i + 3] &&
+        board[i] === board[i + 6]
+      ) {
+        return true;
+      }
+    }
+
+
+    if (
+      board[0] !== "" &&
+      board[0] === board[4] &&
+      board[0] === board[8]
+    ) {
+      return true;
+    }
+    if (
+      board[2] !== "" &&
+      board[2] === board[4] &&
+      board[2] === board[6]
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+
+  function checkDraw(board) {
+    return !board.includes("");
+}
+
+
+
+
 function App() {
   const [showHelp, setShowHelp] = useState(false)
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
@@ -25,6 +127,7 @@ function App() {
   const [player2Score, setPlayer2Score] = useState(0)
   const [roundMessage, setRoundMessage] = useState(false)
   const [previousPlayer, setPreviousPlayer] = useState("")
+  const [isAITurn, setAITurn] = useState(false)
  
   const playerSymbol = ["X", "O"]
 
@@ -77,14 +180,28 @@ function App() {
       newBoard[slot] = currentPlayer;
       setBoard(newBoard);
       setPreviousPlayer(currentPlayer);
-      setCurrentPlayer((prevPlayer) => (prevPlayer === player1 ? player2 : player1));
+      setCurrentPlayer((prevPlayer) => prevPlayer === player1 ? player2 : player1);
       winCheck();
+
+      if(isFirstToThree && !finish && currentPlayer === player2) {
+        setAITurn(true)
+      }
     }
   };
 
   useEffect(() => {
     setCurrentPlayer(player1)
   }, [player1])
+
+  useEffect(() => {
+    if (isAITurn) {
+      setTimeout(() => {
+        const bestMove = calculateBestMove(board, 0, true)
+        chooseSlot(bestMove.index)
+        setIsAITurn(false)
+      }, 500)
+    }
+  }, [isAITurn])
 
   const winCheck = () => {
     WinCondition.forEach((currPattern) => {
