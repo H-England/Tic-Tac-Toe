4f42d877cdcb81218aaa3150abbd1ca13dbed5d1
Harry England <harryengland666@gmail.com>
2023-08-09 14:06:08 +0100

    Added the ability to change the difficulty so the algorithm is no longer unbeatable.

--- src/Components/Algorithm.js
+++ src/Components/Algorithm.js
@@ -1,66 +1,68 @@
 export function calculateBestMove(board, depth, maximizingPlayer, currentSymbol, player1Symbol, player2Symbol, alpha = -Infinity, beta = Infinity) {
-    const opponentSymbol = currentSymbol === player1Symbol ? player2Symbol : player1Symbol;
-  
-    if (checkWin(board)) {
+  const opponentSymbol = currentSymbol === player1Symbol ? player2Symbol : player1Symbol;
+
+  const difficulty = 100;
+
+  if (checkWin(board)) {
       if (maximizingPlayer) {
-        return { score: -1 };
+          return { score: -difficulty + depth };
       } else {
-        return { score: 1 };
+          return { score: difficulty - depth };
       }
-    } else if (checkDraw(board)) {
+  } else if (checkDraw(board)) {
       return { score: 0 };
-    }
-  
-    const moves = [];
-  
-    for (let i = 0; i < board.length; i++) {
-        if (board[i] === '') {
+  }
+
+  const moves = [];
+
+  for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
           const move = {};
           move.index = i;
-    
+
           board[i] = currentSymbol;
           const result = calculateBestMove(board, depth + 1, !maximizingPlayer, opponentSymbol, player1Symbol, player2Symbol, alpha, beta);
           move.score = result.score;
-    
+
           board[i] = '';
           moves.push(move);
-  
-        if (maximizingPlayer) {
-          alpha = Math.max(alpha, move.score);
-        } else {
-          beta = Math.min(beta, move.score);
-        }
-  
-        if (beta <= alpha) {
-          break;
-        }
+
+          if (maximizingPlayer) {
+              alpha = Math.max(alpha, move.score);
+          } else {
+              beta = Math.min(beta, move.score);
+          }
+
+          if (beta <= alpha) {
+              break;
+          }
       }
-    }
-  
-    let bestMove;
-    if (maximizingPlayer) {
+  }
+
+  let bestMove;
+  if (maximizingPlayer) {
       let bestScore = -Infinity;
       for (let i = 0; i < moves.length; i++) {
-        if (moves[i].score > bestScore) {
-          bestScore = moves[i].score;
-          bestMove = i;
-        }
+          if (moves[i].score > bestScore) {
+              bestScore = moves[i].score;
+              bestMove = i;
+          }
       }
-    } else {
+  } else {
       let bestScore = Infinity;
       for (let i = 0; i < moves.length; i++) {
-        if (moves[i].score < bestScore) {
-          bestScore = moves[i].score;
-          bestMove = i;
-        }
+          if (moves[i].score < bestScore) {
+              bestScore = moves[i].score;
+              bestMove = i;
+          }
       }
-    }
-  
-    return moves[bestMove];
   }
-  
- export function checkWin(board) {
-    const winPatterns = [
+
+  return moves[bestMove];
+}
+
+export function checkWin(board) {
+  const winPatterns = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -69,18 +71,18 @@ export function calculateBestMove(board, depth, maximizingPlayer, currentSymbol,
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
-    ];
-  
-    for (let pattern of winPatterns) {
+  ];
+
+  for (let pattern of winPatterns) {
       const [a, b, c] = pattern;
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return true;
+          return true;
       }
-    }
-  
-    return false;
   }
-  
-  function checkDraw(board) {
-    return !board.includes('');
-  }
+
+  return false;
+}
+
+function checkDraw(board) {
+  return !board.includes('');
+}
